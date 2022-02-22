cd5cc9db809a0195128f1a72b9f5893fad64ebd0
davdmoo <david.mulyawan97@gmail.com>
2022-02-22 11:19:33 +0700

    Add category item widget, categories screen, and category model

---
+++ lib/categories_screen.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+import "./dummy_data.dart";
+import "./category_item.dart";
+
+class CategoriesScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+        .map((cat) => CategoryItem(
+            cat.title,
+            cat.color,
+          )).toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2, // for 200 width, the height is 300 px
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
