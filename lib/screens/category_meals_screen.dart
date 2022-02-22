b03efd1045c42d5189d27896b9a0242c363d7fd7
davdmoo <david.mulyawan97@gmail.com>
2022-02-22 17:28:01 +0700

    Hide menu when popping route using float button

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,23 +1,48 @@
 import "package:flutter/material.dart";
 
 import '../dummy_data.dart';
+import "../models/meal.dart";
 import "../widgets/meal_item.dart";
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = "/category-meals"; // a property which can be accessed without instantiating the class
-  // final String categoryId;
-  // final String categoryTitle;
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
 
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+
+  // @override
+  // void initState() { // won't work if context is needed (throws an error)
+  //   super.initState();
+  // }
+
+  @override
+  void didChangeDependencies() { // will work if context is needed
+    super.didChangeDependencies();
+
+    if (!_loadedInitData) {
+      final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+      final categoryId = routeArgs["id"];
+      categoryTitle = routeArgs["title"];
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+  }
+  
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryTitle = routeArgs["title"];
-    final categoryId = routeArgs["id"];
-    final categoryMeals = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -25,15 +50,16 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(itemBuilder: (ctx, index) {
         return MealItem(
-          id: categoryMeals[index].id,
-          name: categoryMeals[index].name,
-          imageUrl: categoryMeals[index].imageUrl,
-          duration: categoryMeals[index].duration,
-          complexity: categoryMeals[index].complexity,
-          affordability: categoryMeals[index].affordability,
+          id: displayedMeals[index].id,
+          name: displayedMeals[index].name,
+          imageUrl: displayedMeals[index].imageUrl,
+          duration: displayedMeals[index].duration,
+          complexity: displayedMeals[index].complexity,
+          affordability: displayedMeals[index].affordability,
+          removeItem: _removeMeal,
         );
       },
-      itemCount: categoryMeals.length,
+      itemCount: displayedMeals.length,
       ),
     );
   }
--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,8 +2,18 @@ import "package:flutter/material.dart";
 
 import "../widgets/main_drawer.dart";
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = "/filters";
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +22,16 @@ class FiltersScreen extends StatelessWidget {
         title: Text("Filters"),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text("Filters"),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Adjust your meal selection",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          )
+        ],
       ),
     );
   }
